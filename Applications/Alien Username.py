6382f7f5bf2b4a7ff431ed5912dfb881acd958b1
Dikcha Singh <101415059+Dikchaa@users.noreply.github.com>
2022-11-26 18:34:00 +0530

    Create Alien Username.py

---
+++ Applications/Alien Username.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+for i in range(int(input().strip())):
+    data = input().strip()
+    match = re.search(r'^[_.][0-9]+[A-Za-z]*[_]?$', data)
+    if match:
+        print("VALID")
+    else:
+        print("INVALID")
