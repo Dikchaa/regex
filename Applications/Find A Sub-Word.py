9bf29b34f85f0a625285a0d3bfc6af87bed58c56
Dikcha Singh <101415059+Dikchaa@users.noreply.github.com>
2022-11-26 18:32:28 +0530

    Create Find A Sub-Word.py

---
+++ Applications/Find A Sub-Word.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+data = []
+for i in range(int(input().strip())):
+    data.append(input().strip())
+
+for i in range(int(input().strip())):
+    query = input().strip()
+    matches_count = 0
+    for e in data:
+        matches = re.findall(r'[A-Za-z_]'+query+r'[A-Za-z_]', e)
+        matches_count += len(matches)
+
+    print(matches_count)
