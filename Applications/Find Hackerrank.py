9c6a4c9fde5ab4a489a29a660bf46c5b4c55eacd
Dikcha Singh <101415059+Dikchaa@users.noreply.github.com>
2022-11-27 16:58:55 +0530

    Create Find Hackerrank.py

---
+++ Applications/Find Hackerrank.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    for _ in range(n):
+        s = input()
+        if re.match(r'^hackerrank$', s):
+            print(0)
+        elif re.match(r'[ \w]+\shackerrank$', s):
+            print(2)
+        elif re.match(r'^hackerrank\s[ \w]+', s):
+            print(1)
+        else:
+            print(-1)
