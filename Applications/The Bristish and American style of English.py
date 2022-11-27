4dc4cc8c744b50c67c2e0c35c74eacf62fd989f6
Dikcha Singh <101415059+Dikchaa@users.noreply.github.com>
2022-11-27 17:14:13 +0530

    Create The Bristish and American style of English.py

---
+++ Applications/The Bristish and American style of English.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+if __name__ == '__main__':
+    n = int(input())
+    s = '\n'.join(input() for _ in range(n))
+    
+    a = int(input())
+    for _ in range(a):
+        b = input()
+        l = re.findall(b[:len(b)-2] + '[sz]e', s)
+        print(len(l))
